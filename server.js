a5d625019ea70fbbcc8d33ece4b1b830fe60e950
Phikolomzi Tshetsha <phiko.tshetsha@gmail.com>
2025-12-06 11:07:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 import aws from 'aws-sdk';
 
 import User from './Schema/User.js';
-import Blog from './Schema/blog.js';
+import Blog from './Schema/Blog.js';
 
 // ----- MongoDB Connection Reuse -----
 let cached = global.mongoose;
